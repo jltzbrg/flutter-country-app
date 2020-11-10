a6dd20dd874382b2d18d9a61c18feb2c57ebd1a6
Julio Litzenberg <julio.litzenberg@gmail.com>
2020-11-11 00:03:19 +0100

    Use FutureBuilder to parse JSON into MyApp()

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,60 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_country_app/models/country.dart';
+import 'package:flutter_country_app/views/list.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: const Color(0xFF02BB9F),
+      primaryColorDark: const Color(0xFF167F67),
+      accentColor: const Color(0xFF167F67),
+    ),
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  MyAppState createState() => MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyAppState extends State<MyApp> {
+  List data;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(
+            "Country App",
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+        body: Container(
+          child: Center(
+            // Use future builder and DefaultAssetBundle to load the local JSON file
+            child: FutureBuilder(
+                future: DefaultAssetBundle.of(context)
+                    .loadString('assets/country.json'),
+                builder: (context, snapshot) {
+                  List<Country> countries = parseJosn(snapshot.data.toString());
+                  //TODO: Eine Möglichkeit überlegen, wie man mit isNotEmpty umgehen soll
+                  return !countries.isEmpty
+                      ? CountyList(country: countries)
+                      : Center(child: CircularProgressIndicator());
+                }),
+          ),
+        ));
+  }
+
+  List<Country> parseJosn(String response) {
+    if (response == null) {
+      return [];
+    }
+    final parsed =
+        json.decode(response.toString()).cast<Map<String, dynamic>>();
+    return parsed.map<Country>((json) => Country.fromJson(json)).toList();
   }
 }
